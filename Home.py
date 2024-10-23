498b61741ee012f32177dfa5e436d730440d1125
Güven Barış PEKALP <b.pekalp@gmail.com>
2024-10-23 13:10:49 +0300

    Update Home.py

--- Home.py
+++ Home.py
@@ -4,9 +4,9 @@ st.header("Welcome!")
 
 # Use markdown for the entire sentence to properly include the link
 st.markdown(
-    "In this website, you'll find every web app that I developed while working on my [Python-Course](https://github.com/bpekalp/Python-Course) repository."
+    "In this website, you'll find all the web apps that I developed while working on my [Python-Course](https://github.com/bpekalp/Python-Course) repository."
 )
 
-st.write(
-    "I follow the 'Python Mega Course: Learn Python in 60 Days, Build 20 Apps' course by Ardit Sulce on Udemy."
+st.markdown(
+    "I follow the [Python Mega Course](https://www.udemy.com/course/the-python-mega-course) by [Ardit Sulce](https://www.linkedin.com/in/arditsulce)."
 )
